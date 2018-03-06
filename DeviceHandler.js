1024c7f027f9a60a55b4402972ba4a8b9c19639f
Andrew Ksendzoff <dacoconuttman@gmail.com>
2018-03-06 16:44:39 -0600

    testing husky + eslint + lint-staged

--- DeviceHandler.js
+++ DeviceHandler.js
@@ -1,23 +1,20 @@
-const udev = require('udev'); //todo: install libudev-dev
+// const udev = require('udev');
 const serialport = require('serialport');
 const EventEmitter = require('events');
 
 
 class DeviceHandler extends EventEmitter {
-
   constructor() {
     super();
-    this.monitor = udev.monitor();
+    //    this.monitor = udev.monitor();
     this.ports = {};
     serialport.list()
-      .then(devices => {
-        devices.filter(device => {
-          !!device.vendorId;
-        })
-          .forEach(device => {
+      .then((devices) => {
+        devices.filter(device => device.vendorId !== undefined)
+          .forEach((device) => {
             console.log(device);
             const name = device.comName;
-            const port = new SerialPort(device.comName, { baudRate: 9600 });
+            const port = new serialport.SerialPort(device.comName, { baudRate: 9600 });
             const newPort = {};
             newPort[name] = port;
             this.ports = { ...this.ports, ...newPort };
@@ -26,27 +23,31 @@ class DeviceHandler extends EventEmitter {
   }
 
   start() {
-    this.monitor.on('add', device => {
+    this.monitor.on('add', (device) => {
       if (device.SUBSYTEM === 'tty' && !this.devices[device.DEVNAME]) {
         const tmpDevice = {};
         const tmpName = device.DEVNAME;
-        const port = new SerialPort(tmpName, { baudRate: 9600 });
+        const port = new serialport.SerialPort(tmpName, { baudRate: 9600 });
         tmpDevice[tmpName] = port;
         this.ports = { ...this.ports, ...tmpDevice };
         console.log(`added ${tmpName}`);
       }
     });
 
-    this.monitor.on('remove', device => {
+    this.monitor.on('remove', (device) => {
       if (device.SUBSYSTEM === 'tty') {
         delete this.ports[device.DEVNAME];
         console.log(`deleted ${device.DEVNAME}`);
       }
     });
 
-    Object.values(this.ports).forEach(port => {
-      port.on('data', data => {
+    Object.values(this.ports).forEach((port) => {
+      port.on('data', (data) => {
         this.emit('data', data);
+      });
+
+      port.on('error', (err) => {
+        this.emit('error', err);
       });
     });
   }
--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const DeviceHandler = require('./DeviceHandler');
 const deviceHandler = new DeviceHandler();
 deviceHandler.start();
 
-deviceHandler.on('data', data => {
+deviceHandler.on('data', (data) => {
   console.log(data);
+});
+
+deviceHandler.on('error', (err) => {
+  console.error(err);
 });
