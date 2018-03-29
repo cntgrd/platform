c96dc3b7af4a3d1e8f0eba70375936bb91e5d448
Yichen Duan <vilnatar@gmail.com>
2018-03-29 16:38:13 -0500

    get and post mock

--- mockserver_test/NodeJS/test.js
+++ mockserver_test/NodeJS/test.js
@@ -5,14 +5,21 @@ const deviceHandler = new DummyHandler();
 
 console.log(deviceHandler.data);
 
+const url = 'https://cf4f5b8e-99ee-46f1-96fb-428394f44234.mock.pstmn.io';
+
 superagent
-    .get('https://cf4f5b8e-99ee-46f1-96fb-428394f44234.mock.pstmn.io/getsth')
+    .get(url + '/getsth')
     .end((err, res) => {
         if (err) { return console.log(err); }
         console.log(res.body);
     });
 
-
+superagent
+    .post(url + '/postpb')
+    .end((err,res) => {
+        if (err) { return console.log(err); }
+        console.log(res.body);
+    });
 // deviceHandler.on('data', (data) => {
 //   console.log(data);
 // });
