b88633f0c1fdec6d79bf35be025b0b73a2c75b29
Andrew Ksendzoff <dacoconuttman@gmail.com>
2018-04-06 16:14:22 -0500

    more async debauchery

--- src/index.js
+++ src/index.js
@@ -2,15 +2,14 @@ const { ArduinoHandler, DeviceHandler, DummyHandler } = require('./handlers');
 const SerialPort = require('serialport');
 
 const ports = SerialPort.list().then(ports => {
-  return ports;
-});
-const deviceHandler = new DeviceHandler(ports);
-deviceHandler.start();
+  const deviceHandler = new DeviceHandler(ports);
+  deviceHandler.start();
 
-deviceHandler.on('data', (data) => {
-  console.log(data);
-});
+  deviceHandler.on('data', (data) => {
+    console.log(data);
+  });
 
-deviceHandler.on('error', (err) => {
-  console.error(err);
+  deviceHandler.on('error', (err) => {
+    console.error(err);
+  });
 });
