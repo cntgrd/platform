efc3bf3c5abeb174b8eca57f7bb72400d78abf9e
Andrew Ksendzoff <dacoconuttman@gmail.com>
2018-04-07 17:16:29 -0500

    uuid's are strings

--- src/index.js
+++ src/index.js
@@ -2,18 +2,18 @@ const { ArduinoHandler, DeviceHandler, DummyHandler } = require('./handlers');
 const SerialPort = require('serialport');
 const fs = require('fs');
 
+// onstart function, generates a station UUID if needed
 (() => {
   const uuid = require('uuid/v4')();
   const data = `module.exports = ${uuid};`;
   fs.writeFile('./uuid.js', data, { flag: 'wx' }, (err) => {
     if(err) console.log('uuid already exists');
-    console.log(`new uuid: ${uuid}`);
+    console.log(`new uuid: '${uuid}'`);
   });
 })();
 
 const ports = SerialPort.list().then(ports => {
-  const uuid = require('./uuid.js');
-  const deviceHandler = new DeviceHandler(ports, uuid);
+  const deviceHandler = new DeviceHandler(ports);
   deviceHandler.start();
 
   deviceHandler.on('data', (data) => {
